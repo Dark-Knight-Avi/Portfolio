e9b79c9962220400a78429fadf8a8b4fb756713e
Aritra-CISTUP <aritra@fsid-iisc.in>
2025-04-29 17:26:32 +0530

    prerender

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import prerender from 'vite-plugin-prerender'
+import ssr from 'vite-plugin-ssr/plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react(), ssr(),
     prerender({
       staticDir: 'dist',
       routes: ['/', '/#about', '/#experience', '/#work', '/#reviews', '/#contact']
